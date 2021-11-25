d70e267b38f7c191ed305839df5fc75b24d19e8a
Daniel Fermin <danielfermin@Daniels-MacBook-Pro.local>
2021-11-25 03:44:52 -0800

    Added scrolling functionality for message index

--- frontend/components/messages/messages_index.jsx
+++ frontend/components/messages/messages_index.jsx
@@ -5,15 +5,17 @@ class MessagesIndex extends React.Component {
     render() {
        
         return (
+            <div className="messages-list-container">
+                <ul className="messages-list">
+                    {/* <h3>Messages</h3> */}
+                    {
+                        this.props.messages.map((message, i) => (
+                            <MessagesIndexItem key={i}  message={message} currentUser={this.props.currentUser} author={this.props.users[message.authorId]} updateMessage={this.props.updateMessage} deleteMessage={this.props.deleteMessage} />
+                        ))
+                    }
+                </ul>
+            </div>
             
-            <ul className="messages-list">
-                {/* <h3>Messages</h3> */}
-                {
-                    this.props.messages.map((message, i) => (
-                        <MessagesIndexItem key={i}  message={message} currentUser={this.props.currentUser} author={this.props.users[message.authorId]} updateMessage={this.props.updateMessage} deleteMessage={this.props.deleteMessage} />
-                    ))
-                }
-            </ul>
         )
     }
 }
