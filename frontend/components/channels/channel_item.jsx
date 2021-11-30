a9ded9f5953f683c875cccfa84c54f4a9b7b6130
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-11-29 23:31:40 -0800

    Changed channel item to class based and centered modal for create channel

--- frontend/components/channels/channel_item.jsx
+++ frontend/components/channels/channel_item.jsx
@@ -1,25 +1,44 @@
 import { withRouter } from "react-router";
 import React from "react";
-const ChannelItem = (props) => {
-    const { channel, activeChannelId, deleteChannel} = props;
-    const name = `#${channel.name}`;
-    const handleChannelClick = () => {
-        
-        props.history.push(`/channels/${channel.id}`)
+
+class ChannelItem extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { hovered: false }
+        this.toggleButtons = this.toggleButtons.bind(this)
+        this.handleChannelClick = this.handleChannelClick.bind(this)
+        this.handleDeleteClick = this.handleDeleteClick.bind(this)
+        this.name = `#${this.props.channel.name}`;
     }
 
-    const handleDeleteClick = (channelId) => {
+    
+    handleChannelClick() {
+
+        this.props.history.push(`/channels/${this.props.channel.id}`)
+    }
+
+    handleDeleteClick(channelId) {
         return e => {
             e.stopPropagation();
-            deleteChannel(channelId);
+            this.props.deleteChannel(channelId);
+            this.props.history.push("/home")
         }
-    };
-   
-    return  <li onClick={handleChannelClick} className={ parseInt(activeChannelId) === channel.id ? "active-channel" : ""}>
-        {name}
-        <button>Edit</button>
-        <button onClick={handleDeleteClick(channel.id)}>Delete</button>
-    </li>
+    }
+    toggleButtons() {
+        this.setState({ hovered: this.state.hovered ? false : true })
+    }
+
+    render() {
+        return <li onClick={this.handleChannelClick} onMouseEnter={this.toggleButtons} onMouseLeave={this.toggleButtons} className={parseInt(this.props.activeChannelId) === this.props.channel.id ? "active-channel" : ""}>
+            { this.name }
+            { this.state.hovered ? <div>
+                <button>Edit</button>
+                <button onClick={this.handleDeleteClick(this.props.channel.id)}>Delete</button>
+            </div> :  null }
+
+        </li>
+    }
+
 }
 
 export default withRouter(ChannelItem)
