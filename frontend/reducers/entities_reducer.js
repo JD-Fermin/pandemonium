81114f88a693878a376e4608199270840d6bccb4
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-11-30 10:54:51 -0800

    Added servers reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import channelsReducer from "./channels_reducer";
 import messagesReducer from "./messages_reducer";
+import serversReducer from "./servers_reducer";
 import usersReducer from "./users_reducer";
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     messages: messagesReducer,
-    channels: channelsReducer
+    channels: channelsReducer,
+    servers: serversReducer
 })
 
 export default entitiesReducer;
---
+++ frontend/reducers/servers_reducer.js
@@ -0,0 +1,26 @@
+import { FETCH_SERVER_LIST, CREATE_SERVER, UPDATE_SERVER, DELETE_SERVER } from "../actions/server_actions";
+
+const initialState = {}
+
+const serversReducer = (state = initialState, action) => {
+    Object.freeze(state)
+    let nextState = Object.assign({}, state);
+
+    switch (action.type) {
+        case FETCH_SERVER_LIST:
+            return action.payload;
+        case CREATE_SERVER:
+            nextState[action.payload.id] = action.payload;
+            return nextState;
+        case UPDATE_SERVER:
+            nextState[action.payload.id] = action.payload;
+            return nextState;
+        case DELETE_SERVER:
+            delete nextState[action.payload];
+            return nextState
+        default:
+            return state;
+    }
+}
+
+export default serversReducer
