19da8889c66e0dae961a860bc0fbe494e3208fda
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-12-01 11:36:38 -0800

    added toggle for settings icon

--- frontend/components/servers/server_header.jsx
+++ frontend/components/servers/server_header.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 class ServerHeader extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { openForm: false }
+        this.toggleForm = this.toggleForm.bind(this)
+    }
+
+    toggleForm() {
+        this.setState({ openForm: this.state.openForm ? false : true })
+    }
+
     render() {
         return (
             <div className="server-title">
                 {this.props.activeServer ? <h2>{this.props.activeServer.name}</h2> : null}
-                {this.props.activeServer && this.props.currentUser.id === this.props.activeServer.ownerId ? <span className="server-settings">⚙︎</span> : null}
+                {this.props.activeServer && this.props.currentUser.id === this.props.activeServer.ownerId ? <span onClick={this.toggleForm} className="server-settings">⚙︎</span> : null}
+                {this.state.openForm ? <div className="create-channel-container"></div> : null}
             </div>
         )
     }
