5ab8be5d4c4457a71a1bf9766c3a7d2c2dd31ec7
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-12-01 23:55:28 -0800

    added basic servers index component

--- frontend/components/servers/server_index.jsx
+++ frontend/components/servers/server_index.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchRandomServerList } from "../../actions/server_actions";
-import ServerListItem from "../servers/server_list_item"
+
 class ServerIndex extends React.Component {
 
     componentDidMount() {
         this.props.fetchRandomServers()
     }
-   
+
     render() {
         if (Object.keys(this.props.servers).length === 0) return null;
-        return <ul className='server-list'>
-                {
-                    this.props.servers.map((server) => (
-                        <ServerListItem key={server.id + server.name} server={server}  />
-                    ))
-                }
-        </ul>
+        return <div className="random-servers-container">
+            {
+                this.props.servers.map((server) => (
+                        <div key={server.name + server.id}className="random-server-item">
+                            <h3>{server.name}</h3>
+                            <p>{server.description}</p>
+                        </div>
+                ))
+            }
+        </div>
     }
 }
 
