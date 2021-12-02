1acedd60f873b92044b1cabd69e12c5362cbbdf5
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-12-02 00:25:45 -0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,50 @@ User.create(
     password: "password"
 )
 
+User.create(
+    username: "SuperIdol",
+    email: "rebase@teras.com",
+    password: "123456"
+)
+
+User.create(
+    username: "AmongUs",
+    email: "red9@email.com",
+    password: "123456"
+)
+
+User.create(
+    username: "MakoNugget",
+    email: "mcnugget12@gmail.com",
+    password: "123456"
+)
+
+User.create(
+    username: "theWok",
+    email: "dwaynej@yahoo.com",
+    password: "123456"
+)
+
+User.create(
+    username: "tokaiteio69",
+    email: "teio69@yahoo.com",
+    password: "123456"
+)
+
+User.create(
+    username: "danyelho",
+    email: "danyel777777@yahoo.com",
+    password: "123456"
+)
+
+User.create(
+    username: "Miyako1443",
+    email: "ok@yahoo.com",
+    password: "123456"
+)
+
+
+
 Server.create(
     name: "Da Cultured Chaps",
     description: "place of cultured gentlechaps",
@@ -42,6 +86,26 @@ Server.create(
 
 Membership.create(
     user_id: 2,
+    server_id: 2
+)
+
+Membership.create(
+    user_id: 3,
+    server_id: 2
+)
+
+Membership.create(
+    user_id: 5,
+    server_id: 2
+)
+
+Membership.create(
+    user_id: 2,
+    server_id: 1
+)
+
+Membership.create(
+    user_id: 10,
     server_id: 2
 )
 
@@ -71,3 +135,129 @@ Channel.create(
     name: "general",
     server_id: 2
 )
+
+# 3
+Server.create(
+    name: "Jabee",
+    description: "chickenlicious delish",
+    owner_id: 9
+)
+
+Membership.create(
+    user_id: 9,
+    server_id: 3
+)
+
+Channel.create(
+    name: "general",
+    server_id: 3
+)
+
+#4
+
+Server.create(
+    name: "Coffee Lovers Club",
+    description: "Gathering of the coffe aficionados",
+    owner_id: 6
+)
+
+Membership.create(
+    user_id: 6,
+    server_id: 4
+)
+
+Channel.create(
+    name: "general",
+    server_id: 4
+)
+
+#5
+
+Server.create(
+    name: "Masters of Chaldea",
+    description: "Respite for the wandering saviours",
+    owner_id: 7
+)
+
+Membership.create(
+    user_id: 7,
+    server_id: 5
+)
+
+Channel.create(
+    name: "general",
+    server_id: 5
+)
+
+#6
+
+Server.create(
+    name: "Admirals of the Sea",
+    description: "For the people that dare oppose Neptune",
+    owner_id: 10
+)
+
+Membership.create(
+    user_id: 10,
+    server_id: 6
+)
+
+Channel.create(
+    name: "general",
+    server_id: 6
+)
+
+#7
+Server.create(
+    name: "Horse Racers",
+    description: "derby bois lets go",
+    owner_id: 7
+)
+
+Membership.create(
+    user_id: 7,
+    server_id: 7
+)
+
+Channel.create(
+    name: "general",
+    server_id: 7
+)
+
+#8
+
+Server.create(
+    name: "Oripathy Research Circle",
+    description: "A research circle for studying Oripathy",
+    owner_id: 1
+)
+
+Membership.create(
+    user_id: 1,
+    server_id: 8
+)
+
+Channel.create(
+    name: "general",
+    server_id: 8
+)
+
+#9
+
+Server.create(
+    name: "Captain of the Rails",
+    description: "For those who brave the frontier",
+    owner_id: 3
+)
+
+Membership.create(
+    user_id: 3,
+    server_id: 9
+)
+
+Channel.create(
+    name: "general",
+    server_id: 9
+)
+
+
