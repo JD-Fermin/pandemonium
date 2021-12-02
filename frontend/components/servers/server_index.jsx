690133af2d0ad292480a2610fb1f154506190f3f
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-12-01 20:34:22 -0800

    Added connect for Server index item

--- frontend/components/servers/server_index.jsx
+++ frontend/components/servers/server_index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { fetchServerList } from "../../actions/server_actions";
+import ServerListItem from "../servers/server_list_item"
 class ServerIndex extends React.Component {
+
+   
     render() {
-        return <h1>Hello</h1>
+        if (Object.keys(this.props.servers).length === 0) return null;
+        return <ul className='server-list'>
+                {
+                    this.props.servers.map((server) => (
+                        <ServerListItem key={server.id + server.name} server={server}  />
+                    ))
+                }
+        </ul>
     }
 }
 
-export default ServerIndex
+const mSTP = (state) => ({
+    servers: {}
+})
+
+const mDTP = (dispatch) => ({
+    fetchServers: () => dispatch(fetchServerList())
+})
+
+export default connect(mSTP, mDTP)(ServerIndex)
